ad1eb4fbaa70379293422a9ec55af6888fd450c5
ojha <anubhavojha9@gmail.com>
2021-10-18 20:41:22 +0530

    Initial commit

---
+++ app/src/main/java/com/mazetech/bmicalculator/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.mazetech.bmicalculator
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import com.google.android.gms.ads.*
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+class MainActivity : AppCompatActivity() {
+    lateinit var mAdView : AdView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar!!.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        supportActionBar!!.setCustomView(R.layout.toolbar_center)
+
+        MobileAds.initialize(this) {}
+
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+
+        mAdView.adListener = object: AdListener() {
+            override fun onAdLoaded() {
+                super.onAdLoaded()
+                // Toast.makeText(this@MainActivity, "Add loaded" )
+                // Code to be executed when an ad finishes loading.
+            }
+
+            override fun onAdFailedToLoad(adError : LoadAdError) {
+                // Code to be executed when an ad request fails.
+                super.onAdFailedToLoad(adError)
+                mAdView.loadAd(adRequest)
+            }
+
+            override fun onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+                super.onAdOpened()
+            }
+
+            override fun onAdClicked() {
+                // Code to be executed when the user clicks on an ad.
+                super.onAdClicked()
+            }
+
+            override fun onAdClosed() {
+                // Code to be executed when the user is about to return
+                // to the app after tapping on an ad.
+            }
+        }
+
+        calculateBtn.setOnClickListener {
+
+            if(heightEDT.text.isNotEmpty() && weightEDT.text.isNotEmpty()){
+
+                val weight = weightEDT.text.toString().toDouble()
+                val height = heightEDT.text.toString().toDouble()/100
+
+                if (weight > 0 && weight < 600 && height > 0 && height < 2.5 ) {
+                    val intent = Intent(this@MainActivity, RESULT::class.java)
+                    intent.putExtra("bmi",calculateBMI(weight,height))
+                    startActivity(intent)
+
+                }
+                else
+                {
+                    showError("Incorrect Values")
+                }
+
+            }
+            else
+            {
+                showError("Incorrect Values")
+            }
+        }
+
+    }
+    private fun showError(errorMsg: String)
+    {
+        val snackbar = Snackbar.make(container,"error: $errorMsg !", Snackbar.LENGTH_LONG)
+        snackbar.view.setBackgroundResource(R.color.colorRed)
+        snackbar.show()
+    }
+    private fun calculateBMI(weight: Double, height: Double) = BigDecimal(weight/(height * height))
+        .setScale(2, RoundingMode.HALF_EVEN).toDouble()
+}
---
+++ app/src/main/java/com/mazetech/bmicalculator/RESULT.kt
@@ -0,0 +1,88 @@
+package com.mazetech.bmicalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.google.android.gms.ads.*
+import kotlinx.android.synthetic.main.activity_result.*
+
+class RESULT : AppCompatActivity() {
+    lateinit var mAdView : AdView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+        MobileAds.initialize(this) {}
+
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+
+        mAdView.adListener = object: AdListener() {
+            override fun onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+                super.onAdLoaded()
+            }
+
+            override fun onAdFailedToLoad(adError : LoadAdError) {
+                // Code to be executed when an ad request fails.
+
+                super.onAdFailedToLoad(adError)
+                mAdView.loadAd(adRequest)
+
+            }
+
+            override fun onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+                super.onAdOpened()
+            }
+
+            override fun onAdClicked() {
+                // Code to be executed when the user clicks on an ad.
+                super.onAdClicked()
+            }
+
+            override fun onAdClosed() {
+                // Code to be executed when the user is about to return
+                // to the app after tapping on an ad.
+            }
+        }
+
+        val bmi = intent.getDoubleExtra("bmi", -1.0)
+        if(bmi == -1.0)
+        {
+            containerR.visibility = View.GONE
+        }
+        else{
+            bmiValueTV.text = bmi.toString()
+            if(bmi < 18.5)
+            {
+                containerR.setBackgroundResource(R.drawable.yellow_bg)
+                bmiFlagImage.setImageResource(R.drawable.exclamation_mark)
+                bmiLableTv.text = "You are UnderWeight"
+                commentTV.text = "Here are some advice to help you :"
+                advice1IMG.setImageResource(R.drawable.nowater)
+                advice1TV.text = "Don't drink water before meals"
+                advice2IMG.setImageResource(R.drawable.bigmeal)
+                advice2TV.text = "Use bigger plates"
+                advice3TV.text ="Get quality sleep"
+
+            } else {
+                if (bmi > 25){
+                    containerR.setBackgroundResource(R.drawable.red_bg)
+                    bmiFlagImage.setImageResource(R.drawable.warnings)
+                    bmiLableTv.text = "You are OverWeight"
+                    commentTV.text = "Here are some advice to help you :"
+                    advice1IMG.setImageResource(R.drawable.water)
+                    advice1TV.text = "Don't drink water before meals"
+                    advice2IMG.setImageResource(R.drawable.twoeggs)
+                    advice2TV.text = "Eat only two meals per day and make sure they are high in protein"
+                    advice3IMG.setImageResource(R.drawable.nosugar)
+
+                    advice3TV.text ="Drink coffe or tea and avoid sugary food "
+                }
+            }
+        }
+    }
+
+}
